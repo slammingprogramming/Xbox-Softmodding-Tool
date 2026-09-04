53203aa1c1272468c9374f260233f92557c9f324
Rocky5 <Rocky5@users.noreply.github.com>
2026-09-04 03:51:45 +0100

    Dashloader update:   Bumped to v2.2.2
    Extras disc update:
      Bumped hardmod dashloader version to v2.2.2
      Added latest Dashloader customizer
      Added the latest UIX Lite
      Updated XBMC4Xbox to my latest 3.6 dev build.
      (has fixes and stuff ported from gamers)
    
    Endgame installer:
      Updated the endgame installer and packed-xbe-launcher2 source that is used for the payload.xbe
      Hopefully this version works for all, its smaller and better.

--- Update Skin Version Number.bat
+++ Update Skin Version Number.bat
@@ -22,7 +22,6 @@ Set "Skin17=%CD%\Installer Variants\XBHDM Build\C\installer\skins\Softmod"
 Set "Skin18=%CD%\Installer Variants\Quick Upgrade\Quick Upgrade\skins\Softmod"
 Set "Skin19=%CD%\Installer Variants\Quick Update\Quick Update\skins\Softmod"
 Set "Skin20=%CD%\Installer Variants\UDDAE\resoftmod dash\skins\ReSoftmod Dash"
-Set "Skin21=%CD%\Endgame\payload zip\skins\Softmod"
 
 Set "BuildFileLocation=%CD%\Other"
 
@@ -62,7 +61,6 @@ Call Other\Tools\repl.bat "<Version>%old_version%</Version>" "<Version>%replace%
 Call Other\Tools\repl.bat "<Version>%old_version%</Version>" "<Version>%replace%</Version>" L < "%Skin18%\skin.xml" >"%Skin18%\tmp.xml" & Del "%Skin18%\skin.xml" & rename "%Skin18%\tmp.xml" "skin.xml"
 Call Other\Tools\repl.bat "<Version>%old_version%</Version>" "<Version>%replace%</Version>" L < "%Skin19%\skin.xml" >"%Skin19%\tmp.xml" & Del "%Skin19%\skin.xml" & rename "%Skin19%\tmp.xml" "skin.xml"
 Call Other\Tools\repl.bat "<Version>%old_version%</Version>" "<Version>%replace%</Version>" L < "%Skin20%\skin.xml" >"%Skin20%\tmp.xml" & Del "%Skin20%\skin.xml" & rename "%Skin20%\tmp.xml" "skin.xml"
-Call Other\Tools\repl.bat "<Version>%old_version%</Version>" "<Version>%replace%</Version>" L < "%Skin21%\skin.xml" >"%Skin21%\tmp.xml" & Del "%Skin21%\skin.xml" & rename "%Skin21%\tmp.xml" "skin.xml"
 
 Call Other\Tools\repl.bat "Xbox Softmodding Tool v%old_version%" "Xbox Softmodding Tool v%replace%" L < "README.md" >"tmp.txt" & Del "README.md" & rename "tmp.txt" "README.md"
 Call Other\Tools\repl.bat "Xbox Softmodding Tool v%old_version%" "Xbox Softmodding Tool v%replace%" L < "Changes.txt" >"tmp.txt" & Del "Changes.txt" & rename "tmp.txt" "Changes.txt"
